20754093f60eaf53abd0c4d16e7d12d963c0d4b0
Minh Christian Tran <54357939+Mintra99@users.noreply.github.com>
2023-10-03 14:22:57 +0200

    created services

---
+++ Services/Programs/ProgramService.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MeFitBackend.Data;
+using MeFitBackend.Data.Entities;
+using MeFitBackend.Data.Exceptions;
+using Microsoft.Data.SqlClient;
+
+
+namespace MeFitBackend.Services.Programs
+{
+    public class ProgramService : IProgramService
+    {
+        private readonly MeFitDbContext _context;
+
+        public ProgramService(MeFitDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ICollection<Program>> GetAllAsync()
+        {
+            return await _context.Programs.Include(p => p.Workout).ToListAsync();
+        }
+
+        public async Task<Program> GetByIdAsync(int id)
+        {
+            var prog = await _context.Programs.Where(p => p.Id == id)
+                .Include(p => p.Workout)
+                .FirstAsync();
+
+            if (prog == null)
+            {
+                throw new EntityNotFoundException(nameof(prog), id);
+            }
+
+            return prog;
+        }
+
+        public async Task<Program> AddAsync(Program obj)
+        {
+            await _context.Programs.AddAsync(obj);
+            await _context.SaveChangesAsync();
+            return obj;
+        }
+
+        public async Task DeleteByIdAsync(int id)
+        {
+
+            try
+            {
+                var programsToDelete = await _context.Programs.SingleOrDefaultAsync(p => p.Id == id);
+
+                if (programsToDelete == null)
+                {
+                    throw new EntityNotFoundException(nameof(programsToDelete), id);
+                }
+                else
+                {
+                    _context.Remove(programsToDelete);
+                    await _context.SaveChangesAsync();
+                }
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task<Program> UpdateAsync(Program obj)
+        {
+            try
+            {
+                var prog = await _context.Programs.SingleOrDefaultAsync(p => p.Id == obj.Id);
+
+                if (prog == null)
+                {
+                    throw new EntityNotFoundException(nameof(prog), obj.Id);
+                }
+                else
+                {
+                    prog!.Name = obj.Name;
+                    prog!.Duration = obj.Duration;
+                    prog!.Description = obj.Description;
+                    prog!.Category = obj.Category;
+                    prog!.RecomendedLvl = obj.RecomendedLvl;
+                    prog!.Image = obj.Image;
+
+                    await _context.SaveChangesAsync();
+                    return prog!;
+                }
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ Services/Workouts/WorkoutService.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MeFitBackend.Data;
+using MeFitBackend.Data.Entities;
+using MeFitBackend.Data.Exceptions;
+using Microsoft.Data.SqlClient;
+
+
+namespace MeFitBackend.Services.Workouts
+{
+    public class WorkoutService : IWorkoutService
+    {
+        private readonly MeFitDbContext _context;
+
+        public WorkoutService(MeFitDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ICollection<Workout>> GetAllAsync()
+        {
+            return await _context.Workouts.Include(w => w.Exercise).ToListAsync();
+        }
+
+        public async Task<Workout> GetByIdAsync(int id)
+        {
+            var work = await _context.Workouts.Where(w => w.Id == id)
+                .Include(w => w.Exercise)
+                .FirstAsync();
+
+            if (work == null)
+            {
+                throw new EntityNotFoundException(nameof(work), id);
+            }
+
+            return work;
+        }
+
+        public async Task<Workout> AddAsync(Workout obj)
+        {
+            await _context.Workouts.AddAsync(obj);
+            await _context.SaveChangesAsync();
+            return obj;
+        }
+
+        public async Task DeleteByIdAsync(int id)
+        {
+            try
+            {
+                var workoutToDelete = await _context.Workouts.SingleOrDefaultAsync(w => w.Id == id);
+
+                if (workoutToDelete == null)
+                {
+                    throw new EntityNotFoundException(nameof(workoutToDelete), id);
+                }
+                else
+                {
+                    _context.Remove(workoutToDelete);
+                    await _context.SaveChangesAsync();
+                }
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+        }
+        public async Task<Workout> UpdateAsync(Workout obj)
+        {
+            try
+            {
+                var work = await _context.Workouts.SingleOrDefaultAsync(w => w.Id == obj.Id);
+
+                if (work == null)
+                {
+                    throw new EntityNotFoundException(nameof(work), obj.Id);
+                }
+                else
+                {
+                    work!.Name = obj.Name;
+                    work!.Description = obj.Description;
+                    work!.Category = obj.Category;
+                    work!.RecomendedLvl = obj.RecomendedLvl;
+                    work!.Image = obj.Image;
+                    work!.Duration = obj.Duration;
+                    await _context.SaveChangesAsync();
+                    return work!;
+                }
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
